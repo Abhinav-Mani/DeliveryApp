e2b7446238fd3ca6df08d5d3d65029776a4b0001
Abhinav Mani <abhinav1946@gmail.com>
2020-02-09 13:12:03 +0530

    Status Check Update and Product Changes Added

--- app/src/main/java/com/binarybeasts/deliveryapp/Activities/DeliverDetails.java
+++ app/src/main/java/com/binarybeasts/deliveryapp/Activities/DeliverDetails.java
@@ -1,5 +1,6 @@
 package com.binarybeasts.deliveryapp.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -16,18 +17,22 @@ import com.binarybeasts.deliveryapp.R;
 import com.binarybeasts.deliveryapp.Utility.Checker;
 import com.binarybeasts.deliveryapp.Utility.OPTGenerator;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DeliverDetails extends AppCompatActivity implements View.OnClickListener {
     Button requestFarmerOTP,requestCustomerOTP,verifyFarmerOTP,verifyCustomerOTP,callFarmer,callCustomer,directionFarmer,directionCustomer;
     private String farmerOtp,customerOtp;
+    Button BadQuality;
     DatabaseReference reference;
     OPTGenerator optGenerator;
     DeliveryRequests deliveryRequests;
     Checker checker;
     EditText farmerEnteredOTP,customerEnteredOtp;
-    LinearLayout farmerSection,customerSection;
+    LinearLayout farmerSection,customerSection,badSection,completeSection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +49,51 @@ public class DeliverDetails extends AppCompatActivity implements View.OnClickLis
     }
 
     private void checkFeatures() {
+        farmerSection.setVisibility(View.GONE);
+        customerSection.setVisibility(View.GONE);
         if(!checker.isMapCoordinates(deliveryRequests.getCustomerAddress())){
             directionCustomer.setEnabled(false);
         }
         if(!checker.isMapCoordinates(deliveryRequests.getFarmersAddress())){
             directionFarmer.setEnabled(false);
         }
+        String[] s=deliveryRequests.getUID().split("\\|-\\|-\\|");
+        reference.child("Requests").child(s[1]).child(s[0]).child("status").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String status=(String) dataSnapshot.getValue();
+                Toast.makeText(DeliverDetails.this,"status:-"+status,Toast.LENGTH_LONG).show();
+                if(status.equalsIgnoreCase("Out For Delivery")) {
+                    customerSection.setVisibility(View.GONE);
+                    farmerSection.setVisibility(View.VISIBLE);
+                    completeSection.setVisibility(View.GONE);
+                    badSection.setVisibility(View.GONE);
+                }
+                else if(status.equalsIgnoreCase("Received Product From Farmer")){
+                    customerSection.setVisibility(View.VISIBLE);
+                    farmerSection.setVisibility(View.GONE);
+                    completeSection.setVisibility(View.GONE);
+                    badSection.setVisibility(View.GONE);
+                } else if(status.equalsIgnoreCase("Bad Quality")){
+                    customerSection.setVisibility(View.GONE);
+                    farmerSection.setVisibility(View.GONE);
+                    completeSection.setVisibility(View.GONE);
+                    badSection.setVisibility(View.VISIBLE);
+                } else if(status.equalsIgnoreCase("Delivered")) {
+                    customerSection.setVisibility(View.GONE);
+                    farmerSection.setVisibility(View.GONE);
+                    completeSection.setVisibility(View.VISIBLE);
+                    badSection.setVisibility(View.GONE);
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void setListeners() {
@@ -61,10 +105,14 @@ public class DeliverDetails extends AppCompatActivity implements View.OnClickLis
         callCustomer.setOnClickListener(this);
         directionCustomer.setOnClickListener(this);
         directionFarmer.setOnClickListener(this);
+        BadQuality.setOnClickListener(this);
     }
 
 
     private void init() {
+        badSection=findViewById(R.id.badSection);
+        completeSection=findViewById(R.id.completeSection);
+        BadQuality=findViewById(R.id.BADQuality);
         reference= FirebaseDatabase.getInstance().getReference();
         requestFarmerOTP=findViewById(R.id.requestFarmerOTP);
         requestCustomerOTP=findViewById(R.id.requestCustomerOTP);
@@ -86,13 +134,21 @@ public class DeliverDetails extends AppCompatActivity implements View.OnClickLis
     {
         String text=farmerEnteredOTP.getText().toString().trim();
         if(text.equals(farmerOtp)){
-            //farmerSection.setVisibility(View.GONE);
-            customerSection.setVisibility(View.VISIBLE);
             String[] s = deliveryRequests.getUID().split("\\|-\\|-\\|");
             reference.child("Requests").child(s[1]).child(s[0]).child("status").setValue("Received Product From Farmer");
             //Toast.makeText(this,s[0]+"   "+s[1],Toast.LENGTH_LONG).show();
         }
     }
+
+    private void badQuality()
+    {
+
+            String[] s = deliveryRequests.getUID().split("\\|-\\|-\\|");
+            reference.child("Requests").child(s[1]).child(s[0]).child("status").setValue("Bad Quality");
+            Toast.makeText(this,s[0]+"   "+s[1],Toast.LENGTH_LONG).show();
+
+    }
+
     private void check() {
         Intent intent=getIntent();
         if(intent.hasExtra("data")){
@@ -127,6 +183,8 @@ public class DeliverDetails extends AppCompatActivity implements View.OnClickLis
            verifyCustomerOTP();
        } else if(view==verifyFarmerOTP){
             verifyFarmerOtp();
+       } else if(view==BadQuality){
+            badQuality();
        }
     }
 
--- app/src/main/java/com/binarybeasts/deliveryapp/Utility/Checker.java
+++ app/src/main/java/com/binarybeasts/deliveryapp/Utility/Checker.java
@@ -2,6 +2,8 @@ package com.binarybeasts.deliveryapp.Utility;
 
 public class Checker {
     public boolean isMapCoordinates(String cord){
+        if(cord==null)
+            return false;
         return cord.matches("\\d{0,3}\\.\\d{6,} \\d{0,3}\\.\\d{6,}");
     }
 }
